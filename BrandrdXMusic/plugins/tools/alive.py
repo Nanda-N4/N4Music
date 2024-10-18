1cde7cc4c15514a448db21c8dac2f3e15ecd9a70
N4ND4 <47235064+Nanda-N4@users.noreply.github.com>
2024-10-18 13:21:05 +0630

    Update alive.py

--- BrandrdXMusic/plugins/tools/alive.py
+++ BrandrdXMusic/plugins/tools/alive.py
@@ -15,15 +15,15 @@ async def start(client: Client, message: Message):
             [
                [
             InlineKeyboardButton(
-                text="☆ ʙʀᴀɴᴅᴇᴅ 💗 ", url=f"https://t.me/BRANDRDKING82"
+                text="☆ ʙʀᴀɴᴅᴇᴅ 💗 ", url=f"https://t.me/n4vpn"
             ),
             InlineKeyboardButton(
-                text="☆ ꜱᴜᴘᴘᴏʀᴛ 💗", url=f"https://t.me/BRANDED_WORLD"
+                text="☆ ꜱᴜᴘᴘᴏʀᴛ 💗", url=f"https://t.me/t3amn4chat"
             ),
         ],
                 [
             InlineKeyboardButton(
-                text="☆ ᴄʜᴀɴɴᴇʟ💗", url=f"https://t.me/BRANDRD_BOT"
+                text="☆ ᴄʜᴀɴɴᴇʟ💗", url=f"https://t.me/n4vpn2"
             ),
                 ],
                 [

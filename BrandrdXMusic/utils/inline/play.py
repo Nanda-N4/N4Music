74b6e9d181f76de66dd562624cb5cb342ac5e293
N4ND4 <47235064+Nanda-N4@users.noreply.github.com>
2024-10-18 12:42:27 +0630

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -71,7 +71,7 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
                 text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/n4nd404"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/t3amn4chat"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/n4vpn"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -93,7 +93,7 @@ def stream_markup(_, videoid, chat_id):
                 text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/n4nd404"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/t3amn4chat"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/n4vpn"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -248,7 +248,7 @@ def stream_markup2(_, chat_id):
                 text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/n4nd404"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/t3amn4chat"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/n4vpn"
             ),
         ],
 
